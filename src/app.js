6d07c7b6242fb5fb109686efb1aa3506d7db6213
Coty Sanchez <constanzasanchezdigital@gmail.com>
2023-12-14 16:25:30 -0300

    primer commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const ProductManager = require('./product-manager.js');
+
+const app = express();
+const port = 8080;
+
+const manager =  new ProductManager("./src/productos.json");
+
+// Ruta para obtener todos los productos con límite 
+app.get('/products', async (req, res) => {
+    try{
+        const limit = req.query.limit;
+        const productos = await manager.getProductsLimit(limit);
+        res.json({ products: productos });
+    } catch (error){
+        console.log("Error al obtener productos:",error);
+        res.status(500).json({error: "Error interno del seridor"});
+    }    
+});
+
+// Ruta para obtener un producto por ID
+app.get('/products/:pid', async (req, res) => {
+    try{
+        const productId = parseInt(req.params.pid);
+        const producto = await manager.getProductById(productId);
+        if (producto) {
+            res.json({ product: producto });
+        } else {
+            res.status(404).json({ error: 'Producto no encontrado' });
+        }
+     }catch (error){
+        console.error("Error al obtener producto por ID:",error);
+        res.status(500).json({error: "Error interno del servidor"});
+
+     } 
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Express escuchando en el puerto ${port}`);
+});
